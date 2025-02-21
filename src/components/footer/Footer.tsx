96f1c9f164036da2446604f64dcab75c18be8478
Eduarda <contatoeduardagaleno@outlook.com>
2025-02-20 23:13:31 -0300

    :zap: Criação dos Componentes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,29 @@
-import { LinkedinLogo, InstagramLogo, FacebookLogo } from '@phosphor-icons/react';
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
-  const date = new Date().getFullYear();
+    let data = new Date().getFullYear()
 
-  return (
-    // <div className="w-full flex justify-center bg-indigo-900 text-white fixed bottom-0 left-0">
-    <div className="inset-x-0 flex justify-center fixed bottom-0 bg-indigo-900 text-white">
-      <div className="container flex flex-col items-center py-4">
-        <p className='text-xl font-bold'>
-          Blog Pessoal Generation | Copyright: {date}
-        </p>
-        <p className='text-lg'>
-          Acesse nossas redes sociais
-        </p>
-        <div className='flex gap-2'>
-          <LinkedinLogo size={48} weight='bold' />
-          <InstagramLogo size={48} weight='bold' />
-          <FacebookLogo size={48} weight='bold' />
-        </div>
-      </div>
-
-    </div>
-  )
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>
+                        Acesse nossas redes sociais
+                    </p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/liamatsubara/" target='_blank'><LinkedinLogo size={48} weight='regular' /></a>
+                        
+                        <InstagramLogo size={48} weight='regular' />
+                        <FacebookLogo size={48} weight='regular' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,38 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
-  return (
-    <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-      <div className="container flex justify-between text-lg">
-        <p className="ml-4"><Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link></p>
 
-        <div className="flex">
-          <nav>
-            <ul className="flex text-center list-none">
-              <li className="mr-4">Postagens</li>
-              <li className="mr-4">Temas</li>
-              <li className="mr-4">Cadastrar Tema</li>
-              <li className="mr-4">Perfil</li>
-              <li className="mr-4"><Link to="/login">Sair</Link></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-  )
+    const navigate = useNavigate()
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+    return (
+        <>
+            <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+            
+                <div className="container flex justify-between text-lg">
+                    <Link to="/home" className="text-2xl font-bold">
+                        Blog Pessoal
+                    </Link>
+
+                    <div className="flex gap-4 px-4">
+                         <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to="" onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
